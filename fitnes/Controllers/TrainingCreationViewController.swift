777bde689d69d5d3e981b67fe26458ef4de51d6f
Yauheni Prakapenka <eprakopenko@intervale.ru>
2020-07-13 23:00:49 +0300

    Initial commit

---
+++ fitnes/Controllers/TrainingCreationViewController.swift
@@ -0,0 +1,9 @@
+//
+//  TrainingCreationViewController.swift
+//  fitnes
+//
+//  Created by yauheni prakapenka on 13.07.2020.
+//  Copyright © 2020 yauheni prakapenka. All rights reserved.
+//
+
+import Foundation
---
+++ fitnes/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fitnes
+//
+//  Created by yauheni prakapenka on 13.07.2020.
+//  Copyright © 2020 yauheni prakapenka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
