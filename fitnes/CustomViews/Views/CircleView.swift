7fea764735468efd9927e010e7fae4538807e6a6
Yauheni Prakapenka <eprakopenko@intervale.ru>
2020-07-30 22:55:52 +0300

    Improved profile screen

---
+++ fitnes/CustomViews/Views/CircleView.swift
@@ -0,0 +1,31 @@
+//
+//  RoundView.swift
+//  fitnes
+//
+//  Created by yauheni prakapenka on 30.07.2020.
+//  Copyright © 2020 yauheni prakapenka. All rights reserved.
+//
+
+import UIKit
+
+class CircleView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(x: Int, y: Int, size: Int, color: UIColor) {
+        super.init(frame: CGRect(x: x, y: y, width: size, height: size))
+        backgroundColor = color
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = self.frame.size.width / 2
+    }
+}
