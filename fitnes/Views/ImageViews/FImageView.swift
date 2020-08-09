160b76b35da72da8964766060e11fff635234b7c
Yauheni Prakapenka <eprakopenko@intervale.ru>
2020-08-09 10:36:31 +0300

    Improved calendar

---
+++ fitnes/Views/ImageViews/FImageView.swift
@@ -0,0 +1,27 @@
+//
+//  FNoTrainingImageView.swift
+//  fitnes
+//
+//  Created by yauheni prakapenka on 09.08.2020.
+//  Copyright © 2020 yauheni prakapenka. All rights reserved.
+//
+
+import UIKit
+
+class FImageView: UIImageView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        clipsToBounds = true
+        contentMode = .scaleAspectFit
+    }
+}
