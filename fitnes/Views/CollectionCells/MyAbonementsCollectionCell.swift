2e5d2e020ae46b3c4ba37adbed2836ade6c27baf
Yauheni Prakapenka <eprakopenko@intervale.ru>
2020-08-09 14:10:27 +0300

    Added timer

--- fitnes/Views/CollectionCells/MyAbonementsCollectionCell.swift
+++ fitnes/Views/CollectionCells/MyAbonementsCollectionCell.swift
@@ -20,13 +20,17 @@ class MyAbonementsCollectionCell: UICollectionViewCell {
     }
     
     let abonementLabel = FLabel(textAligment: .left, fontSize: 15, weight: .regular, color: .white, message: "")
-    let costLabel = FLabel(textAligment: .left, fontSize: 22, weight: .semibold, color: .black, message: "")
+    let costLabel = FLabel(textAligment: .left, fontSize: 16, weight: .semibold, color: .white, message: "")
     var status = ""
+    
+    var abonementTimer = 59
+    let timerLabel = FLabel(textAligment: .left, fontSize: 12, weight: .light, color: .white, message: "")
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         configureLayout()
         configureUI()
+        addAbonementTimer()
     }
     
     required init?(coder: NSCoder) {
@@ -36,12 +40,17 @@ class MyAbonementsCollectionCell: UICollectionViewCell {
     private func configureLayout() {
         contentView.addSubview(abonementLabel)
         contentView.addSubview(costLabel)
+        contentView.addSubview(timerLabel)
         
         abonementLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10).isActive = true
         abonementLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10).isActive = true
         abonementLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10).isActive = true
         
-        costLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10).isActive = true
+        timerLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10).isActive = true
+        timerLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10).isActive = true
+        timerLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10).isActive = true
+        
+        costLabel.bottomAnchor.constraint(equalTo: timerLabel.topAnchor, constant: -5).isActive = true
         costLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10).isActive = true
         costLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10).isActive = true
     }
@@ -53,6 +62,7 @@ class MyAbonementsCollectionCell: UICollectionViewCell {
         costLabel.numberOfLines = 1
         costLabel.adjustsFontSizeToFitWidth = true
         costLabel.minimumScaleFactor = 0.5
+        
         contentView.layer.cornerRadius = 10
     }
     
@@ -65,11 +75,22 @@ class MyAbonementsCollectionCell: UICollectionViewCell {
         case "pink":
             currentColor = #colorLiteral(red: 1, green: 0.5700934529, blue: 0.6348041892, alpha: 1)
         case "orange":
-            currentColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+            currentColor = #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)
         default:
             currentColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
         }
         
         return currentColor
+    }
+    
+    private func addAbonementTimer() {
+        
+        _ = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { timer in
+            if self.abonementTimer == 1 {
+                self.abonementTimer = 59
+            }
+            self.abonementTimer -= 1
+            self.timerLabel.text = "4 д 16 мин \(self.abonementTimer) сек"
+        })
     }
 }
