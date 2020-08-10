da684fc0edf903a144271df9f25d3381539b6e2e
Yauheni Prakapenka <eprakopenko@intervale.ru>
2020-08-10 12:56:04 +0300

    improved chat

--- fitnes/Screens/ChatViewController.swift
+++ fitnes/Screens/ChatViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 struct ChatModel {
     var isOutgoing: Bool
-    var message: String
+    var messageText: String
+    var messageImage: UIImage?
 }
 
 class ChatViewController: UIViewController {
@@ -26,10 +27,12 @@ class ChatViewController: UIViewController {
     let trainerNameLabel = FLabel(textAligment: .center, fontSize: 12, weight: .regular, color: .gray, message: "")
     
     let avatarImageView = UIImageView()
+    let messageImageView = UIImageView()
     
     var chatModel: [ChatModel] = [
-        ChatModel(isOutgoing: true, message: "Добрый вечер Юля! Мы с вами уже сегодня общались. Могу я получить программу тренировки qr коду?"),
-        ChatModel(isOutgoing: false, message: "Добрый вечер! Вот программа. Сканируйте код и отмечайте выполненные задания")
+        ChatModel(isOutgoing: true, messageText: "Добрый вечер!\n\nСбросьте, пожалуйста, qr код с программой тренировки"),
+        ChatModel(isOutgoing: false, messageText: "Привет, вот код.."),
+        ChatModel(isOutgoing: false, messageText: "", messageImage: #imageLiteral(resourceName: "testQR"))
     ]
     
     override func viewDidLoad() {
@@ -45,8 +48,6 @@ class ChatViewController: UIViewController {
         
         configureChatTextField()
         configureTableView()
-        
-        print(selectedTrainer)
     }
     
     private func configureHeaderView() {
@@ -129,7 +130,11 @@ extension ChatViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ChatCell
         cell.isOutgoing = chatModel[indexPath.row].isOutgoing
-        cell.messageLabel.text = chatModel[indexPath.row].message
+        cell.messageLabel.text = chatModel[indexPath.row].messageText
+        
+        if let image = chatModel[indexPath.row].messageImage {
+            cell.messageImageView.image = image
+        }
         
         return cell
     }
@@ -139,51 +144,65 @@ class ChatCell: UITableViewCell {
     
     var isOutgoing = true
     let messageLabel = FLabel(textAligment: .left, fontSize: 17, weight: .regular, color: .black, message: "")
-    let backgroundMessageView = UIView()
+    let containerView = UIView()
+    let messageImageView = UIImageView()
     
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
         
         configureBackgroundMessageView()
         configureMessageLabel()
+        configureMessageImageView()
     }
     
     private func configureBackgroundMessageView() {
-        addSubview(backgroundMessageView)
-        backgroundMessageView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(containerView)
+        containerView.translatesAutoresizingMaskIntoConstraints = false
         
-        backgroundMessageView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-//        backgroundMessageView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
-//        backgroundMessageView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
-        backgroundMessageView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
-        backgroundMessageView.layer.cornerRadius = 10
+        containerView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        containerView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        containerView.layer.cornerRadius = 10
         
         if isOutgoing {
-            backgroundMessageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 160).isActive = true
-            backgroundMessageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10).isActive = true
-            backgroundMessageView.backgroundColor = #colorLiteral(red: 0, green: 0.8082893491, blue: 0.2883248627, alpha: 1)
+            containerView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 160).isActive = true
+            containerView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10).isActive = true
+            containerView.backgroundColor = #colorLiteral(red: 0, green: 0.8082893491, blue: 0.2883248627, alpha: 1)
         } else {
-            backgroundMessageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10).isActive = true
-            backgroundMessageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -160).isActive = true
-            backgroundMessageView.backgroundColor = #colorLiteral(red: 0.9090023637, green: 0.9137477279, blue: 0.9223148823, alpha: 1)
+            containerView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10).isActive = true
+            containerView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -160).isActive = true
+            containerView.backgroundColor = #colorLiteral(red: 0.9090023637, green: 0.9137477279, blue: 0.9223148823, alpha: 1)
         }
     }
     
     private func configureMessageLabel() {
-        backgroundMessageView.addSubview(messageLabel)
+        containerView.addSubview(messageLabel)
         messageLabel.translatesAutoresizingMaskIntoConstraints = false
-        messageLabel.topAnchor.constraint(equalTo: backgroundMessageView.topAnchor).isActive = true
-        messageLabel.bottomAnchor.constraint(equalTo: backgroundMessageView.bottomAnchor).isActive = true
+        messageLabel.topAnchor.constraint(equalTo: containerView.topAnchor).isActive = true
+        messageLabel.bottomAnchor.constraint(equalTo: containerView.bottomAnchor).isActive = true
         if isOutgoing {
-            messageLabel.leadingAnchor.constraint(equalTo: backgroundMessageView.leadingAnchor, constant: 8).isActive = true
-            messageLabel.trailingAnchor.constraint(equalTo: backgroundMessageView.trailingAnchor, constant: -8).isActive = true
+            messageLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 8).isActive = true
+            messageLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -8).isActive = true
             messageLabel.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         } else {
-            messageLabel.leadingAnchor.constraint(equalTo: backgroundMessageView.leadingAnchor, constant: 8).isActive = true
-            messageLabel.trailingAnchor.constraint(equalTo: backgroundMessageView.trailingAnchor, constant: -8).isActive = true
+            messageLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 8).isActive = true
+            messageLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -8).isActive = true
             messageLabel.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         }
         
         messageLabel.numberOfLines = 0
+    }
+    
+    private func configureMessageImageView() {
+        guard let image = messageImageView.image else { return }
+        
+        containerView.addSubview(messageImageView)
+        messageImageView.translatesAutoresizingMaskIntoConstraints = false
+        messageImageView.topAnchor.constraint(equalTo: containerView.topAnchor).isActive = true
+        messageImageView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor).isActive = true
+        messageImageView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor).isActive = true
+        messageImageView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor).isActive = true
+        messageImageView.heightAnchor.constraint(equalToConstant: 220).isActive = true
+        messageImageView.image = image
+        messageImageView.contentMode = .scaleAspectFit
     }
 }
