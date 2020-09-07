6ed81535089a6b3b14956d93994b948af690c5a7
Yauheni Prakapenka <eprakopenko@intervale.ru>
2020-09-08 00:55:37 +0300

    Added set avatar method

---
+++ fitnes/Utils/SetAvatarImage.swift
@@ -0,0 +1,9 @@
+//
+//  SetAvatarImage.swift
+//  fitnes
+//
+//  Created by yauheni prakapenka on 08.09.2020.
+//  Copyright © 2020 yauheni prakapenka. All rights reserved.
+//
+
+import Foundation
